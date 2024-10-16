964b5dab5b101188acaa645a4a32c96f000fa20b
Njihia097 <njihiaian015@gmail.com>
2024-10-16 10:22:05 +0300

    changed house dimensions

--- main.py
+++ main.py
@@ -2,39 +2,28 @@ import cairo
 import math
 
 def main():
-    surface =cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1000)
+    surface =cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1090)
     context = cairo.Context(surface)
     context.set_source_rgb(0.8, 0.8, 0.8)
     context.paint()
 
-    context.set_source_rgb(0, 0, 0.4)
-    context.set_line_width(5)
+    # context.set_source_rgb(0, 0, 0.4)
+    # context.set_line_width(5)
 
 
-
-    context.move_to(100, 350)
-    context.line_to(600, 350)#900, 350
-    context.arc(350, 350, 150, math.pi, 0)
-    context.line_to(900, 350)
-    context.line_to(900, 500)
-    context.line_to(800, 500)
-    context.line_to(800, 950)
-    context.line_to(200, 950)
-    context.line_to(200, 500)
+    # House Shape
+    context.move_to(100, 500)
+    context.line_to(950, 500)
+    context.line_to(950, 600)
+    context.line_to(850, 600)
+    context.line_to(850, 1050)
+    context.line_to(200, 1050)
+    context.line_to(200, 600)
+    context.line_to(100, 600)
     context.line_to(100, 500)
-
-    # context.line_to(600, 350)  # 900, 350
-    # context.line_to(600, 450)  # 900, 500
-    # context.line_to(550, 450)  # 800, 500
-    # context.line_to(550, 700)  # 800, 950
-    # context.line_to(150, 700)  # 200, 950
-    # context.line_to(150, 450)  # 200, 500
-    # context.line_to(100, 450)  # 100, 500
-
-
-
-
-
+    context.set_line_width(3)
+    context.set_source_rgb(0, 0.7, 1)
+    context.stroke()
 
     context.close_path()
     context.stroke()
